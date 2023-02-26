e487d80eac8d384eac610b0e903c5eeb8ba44655
mateo <mateo.guenot20@gmail.com>
2023-02-26 11:00:48 +0100

    Ajout compétences styles et début projets

--- views/competences.php
+++ views/competences.php
@@ -10,63 +10,95 @@
 </div>
 <section class="allsae">
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s1.01 : Implémentation d'un besoin client</span>
       <span>Langage C</span>
     </div>
+    <div>
+      <p>Durant cette SAÉ, j’ai eu l’occasion de travailler dans le langage C avec l’utilisation de pointeur, de gestion de l’allocation mémoire et avec des classes abstraites notamment. Elle a aussi été ma première collaboration avec mes camarades pour réaliser un gros projet commun. Cela ma acquis des compétences en langage C globalement.</p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s1.02 : Comparaison d'approches algorithmiques</span>
       <span>Langage C, Algorithmique</span>
     </div>
+    <div>
+      <p>Cette SAÉ ma permis de faire de la recherche algorithmique avec mes camarades et d’évaluer un autre projet que le mien et qui ma donc aussi fais voir cette SAÉ d’un autre point de vue que celui de mon groupe. Cela ma acquis des compétences en algorithmique et en code de manière générale.</p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s1.03 : Installation d'un poste pour le développement</span>
       <span>Gestion de comptes Windows</span>
     </div>
+    <div>
+      <p>Ce premier projet ma appris toutes les faces cachées de Windows, ce qui ma aidé à me mettre à l’aise sur mon poste de travail en connaissant ma machine. Cela ma acquis des compétences sur le système d’exploitation Windows.</p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s1.04 : Création d'une BD</span>
       <span>Conception BD power AMC</span>
     </div>
+    <div>
+      <p>Cette SAÉ ma permis de découvrir et prendre en main des logiciels de conception tel que power AMC. Cela ma acquis des compétences en matière de conception de diagramme d’utilisation.</p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s1.05 : Recueil des besoins</span>
       <span>Langage HTML/CSS</span>
     </div>
+    <div>
+      <p>
+      Durant ce projet de réalisation d’un site web cherchant à faire découvrir le BUT, j’ai réussi à travailler en groupe en utilisant des logiciels de regroupement de travail tel que GIT. J’ai eu l’occasion de découvrir les bases de l’HTML et toutes les règles CSS pour réaliser un code propre et respectant le W3C ainsi que l’environnement. Cela ma donc acquis des compétences dans le langage HTML ainsi que de réaliser du code propre. 
+      </p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s1.06 : Découverte de l'environnement économique et écologique</span>
       <span>Recherche sur Internet, Développement de paragraphes argumentaires</span>
     </div>
+    <div>
+      <p>Cette SAÉ ma fait découvrir les côtés néfaste de l’informatique avec ses défauts environnementaux. C’est pour cela que, en groupe, nous avons réussi à bien séparer la grosse quantité de travail que nous avions pour nous le répartir équitablement et ainsi faire des recherches approfondies sur notre partie de travail. Cela ma donc acquis des compétences en termes de communications en sein d’un groupe ainsi que, secondement, la recherche bibliographique. </p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s2.01, SAÉ s2.02, SAÉ s2.05 : GraphMap Analysis : implémentation d'un besoin client</span>
       <span>Langage orienté object tel que Java et recherche de librairies pour le développement</span>
     </div>
+    <div>
+      <p></p>
+    </div>
   </div>
   <div class="sae">
-    <div>
+    <div class="title">
       <span>SAÉ s2.03 : Installation de services Réseau</span>
       <span></span>
     </div>
-  </div>
-  <div class="sae">
     <div>
-      <span>SAÉ s2.04 : PowerBI et exploitation de Données</span>
-      <span>Logiciel PorwerBI et traitemenent de données massive</span>
+      <p></p>
     </div>
   </div>
   <div class="sae">
+    <div class="title">
+      <span>SAÉ s2.04 : PowerBI et exploitation de Données</span>
+      <span>Logiciel PorwerBI et traitemenent de données massive</span>
+    </div>
     <div>
+      <p></p>
+    </div>
+  </div>
+  <div class="sae">
+    <div class="title">
       <span>SAÉ s2.06 : Organisation d'un travail d'équipe</span>
       <span></span>
+    </div>
+    <div>
+      <p></p>
     </div>
   </div>
 </section>
