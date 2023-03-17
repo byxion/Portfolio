f4e6173c1992fcc0b67a3a258ea8fb7ba67f831a
mateo <mateo.guenot20@gmail.com>
2023-03-17 22:06:51 +0100

    Changement page d'accueil

--- assets/scripts/accueil.js
+++ assets/scripts/accueil.js
@@ -1,13 +1,21 @@
 // TypeWriter effect
-const animh1 = document.querySelector("h1");
+const animh1 = document.getElementById('toWrite');
 
 new Typewriter(animh1, {
 })
-  .pauseFor(800)
+.pauseFor(800)
 .changeDelay(50)
-.typeString("Bonjour,")
+.typeString("Dév Full-Stack ")
 .pauseFor(200)
-.typeString(" je m'appelle Matéo Guenot.")
+.deleteChars(11)
+.typeString("<span style='color: #FF5154'>HTML</span> / <span style='color: #2D7DD2'>CSS</span>, ")
+.pauseFor(200)
+.deleteChars(12)
+.typeString("<span style='color: #FAFF7F'>JavaScript</span>, ")
+.pauseFor(200)
+.deleteChars(12)
+.typeString("<span style='color: #91A6FF'>PHP</span>.")
+.pauseFor(200)
 .start()
 
 const langdev = document.querySelector(".language-web");
@@ -24,4 +32,36 @@ new Typewriter(langdev, {
 .pauseFor(200)
 .start()
 
+const welcome = document.querySelector('.welcome');
+const followCircle = document.querySelector('.followCircle');
+const nameToAdd = document.querySelector('#int');
 
+welcome.addEventListener('mousemove', e => {
+  let x = e.pageX - followCircle.clientWidth / 2;
+  let y = e.pageY - followCircle.clientHeight / 2;
+  
+  // Vérifie que le cercle ne dépasse pas la limite de l'écran
+  const maxX = welcome.clientWidth - followCircle.clientWidth;
+  const maxY = welcome.clientHeight - followCircle.clientHeight;
+  x = Math.max(0, Math.min(x, maxX));
+  y = Math.max(0, Math.min(y, maxY));
+
+  followCircle.style.left = `${x}px`;
+  followCircle.style.top = `${y}px`;
+
+  nameToAdd.addEventListener('mouseover', e => {
+    followCircle.style.opacity = `0.4`;
+  });
+});
+
+welcome.addEventListener('mouseleave', e => {
+  followCircle.style.left = `-200px`;
+  followCircle.style.top = `-200px`;
+});
+
+const toAboutButton = document.querySelector('.toAbout');
+const toAbout = document.querySelector('.about');
+
+toAboutButton.addEventListener('click', e => {
+  toAbout.scrollIntoView({behavior: "smooth"});
+})
--- views/accueil.php
+++ views/accueil.php
@@ -4,15 +4,19 @@
   <link rel="stylesheet" href="<?= PATH_CSS ?>accueil.css">
   <script defer src="https://unpkg.com/typewriter-effect@latest/dist/core.js">
   </script>
+  <script defer src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
   <script defer type="module" src="<?= PATH_SCRIPTS ?>accueil.js"></script>
   <script defer src="<?= PATH_SCRIPTS ?>navMenu.js" type="module"></script>
 </head>
 <section class="welcome">
-  <h1></h1>
-  <span class="welcome-sentence">Bienvenue sur mon site portfolio. </span>
-  <span class="welcome-sentence order">
-    Vous pouvez scroller pour commencer le site !
-  </span>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="followCircle"></div>
+  <h1>Je suis <span id="name">Maté<span id="int">o</span> Guenot</span>, étudiant en BUT informatique, passionné par les nouvelles technologies et <span id="toWrite"></span></h1>
+  <button class="toAbout">
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" width="24px" height="24px"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 20L12 4" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 14L11.9375 19.9375V19.9375C11.972 19.972 12.028 19.972 12.0625 19.9375V19.9375L18 14" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+  </button>
 </section>
 <section class="about">
   <div class="about-content">
